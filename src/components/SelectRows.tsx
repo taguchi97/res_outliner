4b356f92ace54b259be3d2262b1d03906d9a2848
Atsushi Taguchi <a.taguchi97@gmail.com>
2020-12-20 08:57:10 +0900

    fix

--- src/components/SelectRows.tsx
+++ src/components/SelectRows.tsx
@@ -1,36 +1,46 @@
 import React from 'react'
 
 type SelectRowsProps = {
-  handleModalClose: ()=>void
+  HeaderRow: string[]
+  HeaderRowOriginal : string[]
+  handleModalClose: () => void
 }
 
-const SelectRows = (props:SelectRowsProps)=>{
-  return(
+const SelectRows = (props: SelectRowsProps) => {
+  const checkboxsDisplay = props.HeaderRow.map((row: string) =>
+    <div>
+      <input type='checkbox' />{row}
+    </div>
+  )
+  const headerRowHidden = props.HeaderRowOriginal.filter(i=>props.HeaderRow.indexOf(i)===-1)
+  const checkboxsHidden = headerRowHidden.map((row: string) =>
+  <div>
+    <input type='checkbox' />{row}
+  </div>
+)
+console.log(headerRowHidden)
+  return (
     <>
-            <button type='button' onClick={props.handleModalClose}>✕</button>
-        <h2>列の表示切り替え</h2>
+      <button type='button' onClick={props.handleModalClose}>✕</button>
+      <h2>列の表示切り替え</h2>
+      <div>
+        <div>表示</div>
         <div>
-          <div>表示</div>
-          <div>
-            <input type="checkbox" />期
-            <input type="checkbox" />テーマ名
-            <input type="checkbox" />サブテーマ名
-            <input type="checkbox" />・・・
+          {checkboxsDisplay}
+        </div>
+      </div>
+      <div>
+        <button type='button'>→</button>
+        <button type='button'>←</button>
+      </div>
+      <div>
+        <div>非表示</div>
+        <div>
+          {checkboxsHidden}
           </div>
-        </div>
-        <div>
-          <button type='button'>→</button>
-          <button type='button'>←</button>
-        </div>
-        <div>
-          <div>非表示</div>
-          <div>
-            <input type="checkbox" />特許
-            <input type="checkbox" />・・・
-          </div>
-        </div>
-        <button type='button'>OK</button>
-        <button type='button' onClick={props.handleModalClose}>キャンセル</button>
+      </div>
+      <button type='button'>OK</button>
+      <button type='button' onClick={props.handleModalClose}>キャンセル</button>
 
     </>
   )
